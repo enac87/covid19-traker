c8f135575d9ee130250bcdfe5328766a42c160c1
EMAGA ALAIN <emaga87@gmail.com>
2021-04-19 00:38:29 +0100

    Updates

---
+++ precache-manifest.c4c448a3d3bb6c994c872cd85961b6f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed534a44060398ab791cbd5be31ba140",
+    "url": "/covid19-traker/index.html"
+  },
+  {
+    "revision": "44777d790d8b83290eda",
+    "url": "/covid19-traker/static/css/main.d9993e5a.chunk.css"
+  },
+  {
+    "revision": "36d9a88df0d52c560a7b",
+    "url": "/covid19-traker/static/js/2.8bd5d975.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-traker/static/js/2.8bd5d975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44777d790d8b83290eda",
+    "url": "/covid19-traker/static/js/main.29c9fb7d.chunk.js"
+  },
+  {
+    "revision": "4b9e7432c9df40316395",
+    "url": "/covid19-traker/static/js/runtime-main.7f8680ae.js"
+  }
+]);
